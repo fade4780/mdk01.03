43ff76d7f6c11e26c0e4db3171883c3a46502c14
Fade <gogashogwards4780@gmail.com>
2024-11-14 14:33:54 +0300

    изменил Task3

--- src/Ekzamen/Task3.java
+++ src/Ekzamen/Task3.java
@@ -1,4 +1,17 @@
 package Ekzamen;
+import java.util.Random;
 
 public class Task3 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] expenses = new int[7];
+        int totalExpenses = 0;
+        System.out.println("Расходики за недельку:");
+        for (int i = 0; i < expenses.length; i++){
+            expenses[i] = 2000 + random.nextInt(5001);
+            totalExpenses += expenses[i];
+            System.out.println("День "+ (i + 1) + ":" + expenses[i] +"Рублей");
+        }
+        System.out.println("Общаие траты за неделю: "+ totalExpenses + "Рублей");
+    }
 }
